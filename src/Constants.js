bc4fc45f762e2b5c360c641d194b87614a2e444e
Deep13 <deep.deepak30@gmail.com>
2024-05-01 21:45:42 +0530

    commit

---
+++ src/Constants.js
@@ -0,0 +1,140 @@
+export const day_title = 'Happy Onam';
+export const banner_key1 = 'ca-app-pub-4717579333914549/1619797684';
+export const banner_key2 = 'ca-app-pub-4717579333914549/1428225992';
+export const banner_key3 = 'ca-app-pub-4717579333914549/3862817641';
+export const interstitial_key1 = 'ca-app-pub-4717579333914549/7610490968';
+export const interstitial_key2 = 'ca-app-pub-4717579333914549/1045082611';
+export const interstitial_key3 = 'ca-app-pub-4717579333914549/8732000940';
+export const language = ['English', 'Malayalam'];
+export const textData = {
+  English: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'May the spirit of Onam remains everywhere Whatever you do, Whatever you think, and Whatever you hope in your life. Wish you a very Happy Onam 2020.',
+      },
+      {
+        id: '002',
+        text:
+          'The Onam Celebration is an occasion for people to remind themselves of the all-pervasive nature of the divine. I wish all your family members and especially you, A Happy Onam!',
+      },
+      {
+        id: '003',
+        text:
+          'Wish you all a euphoric, prosperous, colorful, healthy, wealthy and fun-filled Onam! Let this season brings you all lot of good luck, peace of mind, happiness and all that you wish!',
+      },
+      {
+        id: '004',
+        text:
+          'May God bless you, and the color & lights of Onam fill your home with happiness and joy. HAVE A HAPPY ONAM.',
+      },
+      {
+        id: '005',
+        text:
+          'Onam is the time for pookkalam on the floor; children on swings; tiger clad men with hunters behind them; people enjoying sadyas with family. Let this Onam bring Joy and Prosperity to all. Happy Onam!',
+      },
+      {
+        id: '006',
+        text:
+          'Let this Onam bring Joy and prosperity to you and your loved ones. I wish a Happy Onam to you and your family.',
+      },
+      {
+        id: '007',
+        text:
+          'May the colour and lights of Onam fill your home with happiness and joy. My heartfelt wishes on Onam. Happy Onam! From our family to yours.',
+      },
+      {
+        id: '008',
+        text:
+          'On the happy occasion of Onam, may you have abundance of joy in your live, good health and immense prosperity. Wishing a very happy Onam to you and your entire family.',
+      },
+      {
+        id: '009',
+        text:
+          'Accept our warmest greetings on Onam festival. May you have an enjoyable and fun-filled Onam celebration.',
+      },
+      {
+        id: '010',
+        text:
+          'Wishing that King Mahabali blesses you with all that you desire Happy Onam!',
+      },
+      {
+        id: '011',
+        text:
+          'It’s Onam! So, let’s celebrate the spirit of the harvest festival in all its splendour. Decorate your house with pookalams, listen to the melodious Onappattus and enjoy the auspicious festival of Onam!',
+      },
+      {
+        id: '012',
+        text:
+          'I’m wishing you a life as colourful as pookalam, as festive as this Onam festival, and as prosperous as the bountiful harvest. Happy Onam to you!',
+      },
+    ]
+  }
+  ,
+  Malayalam: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'ഈ ഓണം നിങ്ങൾക്ക് നല്ല ഭാഗ്യം, സമൃദ്ധി, സന്തോഷം എന്നിവ നൽകാം. അതിശയകരമായ ഓണം കഴിക്കുക',
+      },
+      {
+        id: '002',
+        text:
+          'മഹാബലി രാജാവ് നിങ്ങൾ ആഗ്രഹിക്കുന്നതെല്ലാം അനുഗ്രഹിക്കണമെന്ന് ആഗ്രഹിക്കുന്നു. ഹാപ്പി ആശംസകൾ!',
+      },
+      {
+        id: '003',
+        text:
+          'ഈ ഓണം ഉത്സവം സന്തോഷത്തോടെയും സന്തോഷത്തോടെയും ആഘോഷിക്കുക. ഈ പുണ്യ സന്ദർഭത്തിന് ആശംസകൾ! എല്ലാവർക്കും ഓനം ആശംസിക്കുന്നു!',
+      },
+      {
+        id: '004',
+        text:
+          'തറയിൽ പൂക്കളം, കുട്ടികൾ സ്വിംഗുകൾ, കടുവ ധരിച്ച പുരുഷന്മാർ എന്നിവരുടെ പിന്നിൽ വേട്ടക്കാരോടൊപ്പമാണ് ഓണം. കുടുംബത്തോടൊപ്പം സാധനങ്ങൾ ആസ്വദിക്കുന്ന ആളുകൾ. ഈ ഓണം എല്ലാവർക്കും സന്തോഷവും സമൃദ്ധിയും നൽകട്ടെ',
+      },
+      {
+        id: '005',
+        text:
+          'ഇത് ഓണം! അതിനാൽ, കൊയ്ത്തുത്സവത്തിന്റെ ചൈതന്യം അതിന്റെ എല്ലാ ആഡംബരത്തിലും ആഘോഷിക്കാം. നിങ്ങളുടെ വീട് പൂക്കലങ്ങൾ കൊണ്ട് അലങ്കരിക്കുക, മനോഹരമായ ഓനപ്പട്ടസ് ശ്രവിക്കുക, ഓണത്തിന്റെ ഉത്സവം ആസ്വദിക്കുക!',
+      },
+      {
+        id: '006',
+        text:
+          'നാട്ടിലെമ്പാടും ആരവങ്ങൾ ചാർത്തി വീണ്ടും ഒരു പൊന്നോണം. മാവേലി മന്നനെ വരവേൽക്കാൻ നാടും വീടും ഒരുങ്ങുന്ന വേളയിൽ താങ്കൾക്കും കുടുംബത്തിനും എൻ്റെ ഒരായിരം ഓണാശംസകൾ.',
+      },
+      {
+        id: '007',
+        text:
+          'ഓണം പരിപാലിക്കാനുള്ള ഒരു ഉത്സവം മാത്രമല്ല, എന്നാൽ ഒരു ഭൂതകാലത്തിന്റെ പുനർജന്മത്തിനുള്ള ഒരു നിമിഷം സമൃദ്ധിയും നന്മയും.',
+      },
+      {
+        id: '008',
+        text:
+          'ഓണം സ്പിരിറ്റ് യാത്രയിലുടനീളം, ഗ്രാന്റ് ഓണം സദ്യയും സന്തോഷവും ഉള്ള ഓനം ആശംസിക്കുന്നു. നിങ്ങൾക്കും നിങ്ങളുടെ കുടുംബത്തിനും ഓനം ആശംസിക്കുന്നു',
+      },
+      {
+        id: '009',
+        text:
+          'ഈ ഓണം നിങ്ങൾക്ക് നല്ല ഭാഗ്യവും സന്തോഷവും കൈവരുമെന്ന് പ്രതീക്ഷിക്കുന്നു! നിങ്ങൾക്കും നിങ്ങളുടെ കുടുംബത്തിനും നിസ്സാരമായ ഓണം ആശംസിക്കുന്നു!',
+      },
+      {
+        id: '010',
+        text:
+          'ദൈവം നിങ്ങളെ അനുഗ്രഹിക്കുകയും നിങ്ങളുടെ ഹൃദയത്തിൽ സന്തോഷവും സന്തോഷവും നിറയ്ക്കുകയും ചെയ്യട്ടെ. ഓണത്തിന്റെ നിറവും ലൈറ്റുകളും നിങ്ങളുടെ വീട്ടിൽ സന്തോഷവും സന്തോഷവും നിറയ്ക്കട്ടെ. ഏറ്റവും മനോഹരമായ ഓണം കഴിക്കൂ',
+      },
+      {
+        id: '011',
+        text:
+          'ഈ ശുഭദിനം നിങ്ങൾക്ക് നല്ല ആരോഗ്യവും സമ്പത്തും നൽകട്ടെ - ഹാപ്പി ഓണം..',
+      },
+      {
+        id: '012',
+        text:
+          'ഓണത്തിന്റെ നിറവും ലൈറ്റുകളും നിങ്ങളുടെ വീട്ടിൽ സന്തോഷവും സന്തോഷവും നിറയ്ക്കട്ടെ. ഓണത്തിന്റെ തലേദിവസം നിങ്ങൾക്കും നിങ്ങളുടെ കുടുംബത്തിനും എന്റെ ഹൃദയംഗമമായ ആശംസകൾ.',
+      },
+    ]
+  }
+
+};
